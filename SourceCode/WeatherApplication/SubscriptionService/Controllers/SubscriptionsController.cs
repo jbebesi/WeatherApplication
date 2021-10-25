d6fe875a14494f016fbf4368855cce437f05b46a
Janos <Janos.bebesi@live.com>
2021-10-25 21:34:04 +0200

    add subscriptions project

---
+++ SourceCode/WeatherApplication/SubscriptionService/Controllers/SubscriptionsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SubscriptionService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApplication.Shared;
+
+namespace SubscriptionService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubscriptionsController : ControllerBase
+    {
+
+        private readonly ILogger<SubscriptionsController> _logger;
+        private readonly ISubscriptionInterface isubscriptionInterface;
+
+        public SubscriptionsController(ILogger<SubscriptionsController> logger, ISubscriptionInterface isubscriptionInterface)
+        {
+            _logger = logger;
+            this.isubscriptionInterface = isubscriptionInterface;
+        }
+
+        [HttpGet]
+        public Subscription Get()
+        {
+            return this.isubscriptionInterface.GetSubscription("");
+        }
+    }
+}
