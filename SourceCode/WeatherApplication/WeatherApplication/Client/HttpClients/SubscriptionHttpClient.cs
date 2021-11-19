a295aebc94e536fbdc1c82b9e78de5c7fed3d4e5
Janos <Janos.bebesi@live.com>
2021-11-19 07:57:26 +0100

    Http client for subscription page

---
+++ SourceCode/WeatherApplication/WeatherApplication/Client/HttpClients/SubscriptionHttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherApplication.Shared.Dtos.Misc;
+
+namespace WeatherApplication.Client.HttpClients
+{
+    public class SubscriptionHttpClient : HttpClient
+    {
+
+        public async Task<Subscription> GetSubscription(string ID)
+        {
+            return await this.GetFromJsonAsync<Subscription>("Subscriptions");
+        }
+    }
+}
