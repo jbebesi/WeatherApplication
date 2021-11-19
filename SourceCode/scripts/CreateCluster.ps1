0db754963b058b9708602b577822079e450931a8
Janos <Janos.bebesi@live.com>
2021-11-19 07:57:00 +0100

    add script to create devTest environment

---
+++ SourceCode/scripts/CreateCluster.ps1
@@ -0,0 +1,20 @@
+﻿$resourceGroup = "WettaDev1"
+$location = "westeurope"
+$appName = "WettaDevTest"
+
+# Create a resource group
+az group create --name $resourceGroup --location $location
+
+# Create a container registry
+az acr create --resource-group $resourceGroup --name $resourceGroup --sku Basic
+
+
+# Create a Kubernetes cluster
+az aks create \
+    --resource-group $resourceGroup \
+    --name $appName \
+    --node-count 1 \
+    --enable-addons monitoring \
+    --generate-ssh-keys \
+    --kubernetes-version 1.22.2
+
