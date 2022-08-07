565982f68005a43586be00f01714f4a0142bfd61
Janos <Janos.bebesi@live.com>
2022-08-07 10:41:34 +0200

    add GetFromJsonAsync. This function is available in .net6

---
+++ SourceCode/WeatherApplication/WeatherApplication/Shared/Helpers/HttpClientHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Shared.Helpers
+{
+    public static class HttpClientHelpers
+    {
+        public static async Task<T> GetFromJsonAsync<T>(this HttpClient client, string uri)
+        {
+            var response = await client.GetAsync(uri);
+            var respStr = await response.Content.ReadAsStringAsync();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(respStr);
+            
+        }
+        
+    }
+}
